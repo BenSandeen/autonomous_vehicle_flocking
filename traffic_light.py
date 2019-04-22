e972849c23258bf44834d59882c6ce9f9bce6df8
BenSandeen <12025856+BenSandeen@users.noreply.github.com>
2019-04-22 11:04:03 -0600

    Traffic lights display correctly

--- traffic_light.py
+++ traffic_light.py
@@ -24,12 +24,17 @@ class TrafficLight:
 
         self.yellow_duration = 6
 
-        # self.time_until_green_changes = random.randint(20, 30)
-        # self.time_until_yellow_changes = 0
+        # Since lights facing in opposite directions should always change together, we use a single timer for each pair
         self.up_and_down_time_until_light_change = random.randint(20, 30)
-        # self.down_time_until_light_change = random.randint(20, 30)
         self.left_and_right_time_until_light_change = math.inf
-        # self.right_time_until_light_change = random.randint(20, 30)
+
+    def get_rgb_color_value(self, light_color):
+        if light_color == LightColor.green:
+            return GREEN
+        elif light_color == LightColor.yellow:
+            return YELLOW
+        elif light_color == LightColor.red:
+            return RED
 
     def get_light_for_direction_of_travel(self, direction):
         if direction == UP:
@@ -102,12 +107,24 @@ class TrafficLight:
             return math.inf
 
     def draw(self):
-        # x = self.position['x'] * CELLSIZE
-        # y = self.position['y'] * CELLSIZE
-        # car_rect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
-        # pygame.draw.rect(DISPLAYSURF, self.outline_color, car_rect)
-        # # car_inner_rect = pygame.Rect(x + 4, y + 4, CELLSIZE - 8, CELLSIZE - 8)
-        # # pygame.draw.rect(DISPLAYSURF, self.body_color, car_inner_rect)
-        pass
+        x = self.position['x'] * CELLSIZE
+        y = self.position['y'] * CELLSIZE
+        car_rect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
 
+        top_left     = (self.position['x'] * CELLSIZE,                  self.position['y'] * CELLSIZE)
+        top_right    = (self.position['x'] * CELLSIZE + CELLSIZE,       self.position['y'] * CELLSIZE)
+        bottom_left  = (self.position['x'] * CELLSIZE,                  self.position['y'] * CELLSIZE + CELLSIZE)
+        bottom_right = (self.position['x'] * CELLSIZE + CELLSIZE,       self.position['y'] * CELLSIZE + CELLSIZE)
+        center       = (self.position['x'] * CELLSIZE + 0.5 * CELLSIZE, self.position['y'] * CELLSIZE + 0.5 * CELLSIZE)
 
+        # Light for traffic heading up...
+        pygame.draw.polygon(DISPLAYSURF, self.get_rgb_color_value(self.up), [bottom_left, center, bottom_right])
+
+        # ...for traffic heading down...
+        pygame.draw.polygon(DISPLAYSURF, self.get_rgb_color_value(self.down), [top_left, top_right, center])
+
+        # ...for traffic heading left...
+        pygame.draw.polygon(DISPLAYSURF, self.get_rgb_color_value(self.left), [center, top_right, bottom_right])
+
+        # ...for traffic heading right...
+        pygame.draw.polygon(DISPLAYSURF, self.get_rgb_color_value(self.right), [bottom_left, top_left, center])
